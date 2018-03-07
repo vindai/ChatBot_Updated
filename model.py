034aeb01fb9726b218e17893e7e83cecd844e6d5
tiliconveli <team3tiliconveli@gmail.com>
2018-03-07 13:24:54 +0530

    Updated Code

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 from rasa_nlu.model import Interpreter ,Metadata
 from rasa_nlu.config import RasaNLUConfig
 
-config =RasaNLUConfig('E:/chat_demo_updated/Data/config_spacy.json')
+config =RasaNLUConfig('E:/Sample/Data/config_spacy.json')
 
 metadata = Metadata.load('./models/trained')
 
 interpreter = Interpreter.load(metadata, config)
 #
-#for testing purposes
+# for testing purposes
 # result=''
 # while(result!='exit'):
 #     user_says=raw_input("User_says> ")
--- vindai_api.py
+++ vindai_api.py
@@ -1,13 +1,17 @@
 import requests
 import json
 import re
-import redis as redis_store
+import redis
+
+redis_store = redis.StrictRedis("localhost")
+
 
 def Fetch_JobQuestions(jobid,clientid = "VAccess20171",clientname = "ChatBotAccess") :
     try:
         print(jobid)
         jobid = re.sub('"', '', jobid)
-        url = 'http://vind.ai/getjobpreqdetails.asmx/getJobDetails?clientid='+ clientid + '&clientname=' + clientname + '&jobid=' + jobid
+        url = 'http://localhost:49407/getjobpreqdetails.asmx/getJobDetails?clientid='+ clientid + '&clientname=' \
+              + clientname + '&jobid=' + jobid
         r = requests.get(url)
         json_parsed = json.loads(r.text)
 
@@ -22,20 +26,19 @@ def Fetch_JobQuestions(jobid,clientid = "VAccess20171",clientname = "ChatBotAcce
         print("Unable to access vind.ai jobdetails " + r.status_code)
 
 
-def Fetch_Question(preq,question_number,total_question):
+def Fetch_Question(preq,question_number,total_question,Sessionid):
+    print(redis_store.get(Sessionid + ':preqflag'))
     try:
-       #Compared the question_number with the TotalPreQQuestions
-         while int(question_number) <= int(total_question) :
+        # Compared the question_number with the TotalPreQQuestions
+        while int(question_number) <= int(total_question) :
             for every_preq in preq:
-                   if str(every_preq.get("PreQid")) == str(question_number):
-                        print(question_number)
-                        return every_preq.get("PreQQuestion")
-         else:
+                 if str(every_preq.get("PreQid")) == str(question_number):
+                      print(question_number)
+                      return every_preq.get("PreQQuestion")
+        else:
+            redis_store.set(Sessionid + ':flag',"n")
+            return "Thank you! We will get back to you soon...If you have queries kindly leave a message."
 
-            return "Thank you! We will get back to you soon... I can help you if you have any other questions."
-
-
-    except:
-
-        print("Unable to get prequalified question for this job ")
-
+    except Exception as e:
+        print(e)
+        print("Unable to get prequalified question for this job")
